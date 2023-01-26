f970f67c34eda9177c1b16f155245dc42fa2f604
zideajang <zidea2015@163.com>
2023-01-26 17:30:24 +0800

    将 jsx 生成虚拟到渲染到 html

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@
 const React = {
     createElement:(tag,props,...children)=>{
         var element = ({tag,props:{...props,children}})
-        console.log(element);
+        // console.log(element);
         if(typeof tag === 'function'){
             return tag(props);
         }
@@ -10,12 +10,30 @@ const React = {
         return element;
     }
 }
-const a = <div className="react-2023">
-    <h1>Hello React 2023</h1>
-    <p>
-        10 Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Repellat ab, quaerat consectetur officiis omnis repudiandae, 
-        voluptates, aperiam iusto aut fugiat accusantium sint hic blanditiis necessitatibus! 
-        Placeat numquam distinctio similique rerum.
+const App = () => (
+<div className="react-2023">
+    <h1 className="title">Hello React 2023</h1>
+    <input className="input" type="text" placeholder="name" />
+    <p className="text">
+        hope make bug progressin in 2023
     </p>
-</div>;
+</div>);
+
+const render = (reactElement,container)=>{
+    if(['string','number'].includes(typeof reactElement )){
+        container.appendChild(document.createTextNode(String(reactElement)));
+        return;
+    }
+    const actualDomElement = document.createElement(reactElement.tag);
+    if(reactElement.props){
+        Object.keys(reactElement.props).filter(p=>p!== 'children').forEach(p=>actualDomElement[p] = reactElement.props[p])
+    }
+
+    if(reactElement.props.children){
+        reactElement.props.children.forEach(child=>render(child,actualDomElement));
+    }
+
+    container.appendChild(actualDomElement);
+}
+
+render(<App/>,document.querySelector("#root"))
--- readme.md
+++ readme.md
@@ -9,9 +9,7 @@ npm install -g yarn
 ```shell
 yarn add typescript parcel -D
 ```
-学习一个框架或者库方式，就是自己去实现这个库或者框架
-从小喜欢解构一些小玩具，就好奇这些小玩具是如果 work
-我们解释 hooks 
+学习一个框架或者库方式，就是自己去实现这个库或者框架从小喜欢解构一些小玩具，就好奇这些小玩具是如果 work 我们解释 hooks 
 
 ```
 yarn parcel index.html
